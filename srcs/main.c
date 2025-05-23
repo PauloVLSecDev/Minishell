c37264f521c73426db69575d49c1f2c7ef965cb1
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-05-23 15:48:58 -0300

    readline working

--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,18 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 19:04:01 by brunogue          #+#    #+#             */
-/*   Updated: 2025/05/23 15:21:17 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2025/05/23 15:46:36 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int ac, char **av)
+int main(void)
 {
     const char	*name;
+	char		*imput;
 
-    if (ac == 2)
-        return (0);
-
-    name = "minishell> ";
-    name = readline(*av);
+    name = "Minishell> ";
+    imput = readline(name);
 	return (0);
 }
