a577a4805189fac998787bf5ac8625af3ad14254
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2025-05-23 15:21:47 -0300

    fix: main for test

--- srcs/main.c
+++ srcs/main.c
@@ -6,17 +6,20 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 19:04:01 by brunogue          #+#    #+#             */
-/*   Updated: 2025/05/23 14:25:54 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/05/23 15:21:17 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int main(int ac, char **av)
 {
-	const char	*name;
+    const char	*name;
 
-	name = "minishell> ";
-	name = readline(name);
+    if (ac == 2)
+        return (0);
+
+    name = "minishell> ";
+    name = readline(*av);
 	return (0);
 }
