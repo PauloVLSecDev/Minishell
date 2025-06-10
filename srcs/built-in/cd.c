ebce8c7e2d3933b99cb2cacd25a12b349fb86b48
Bruno Nogueira De Queiroz <bqueiroz.nog@gmail.com>
2025-06-10 14:56:59 -0300

    working on cd.c

--- srcs/built-in/cd.c
+++ srcs/built-in/cd.c
@@ -6,25 +6,20 @@
 /*   By: brunogue <brunogue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 12:45:10 by brunogue          #+#    #+#             */
-/*   Updated: 2025/06/10 14:19:49 by brunogue         ###   ########.fr       */
+/*   Updated: 2025/06/10 14:46:20 by brunogue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	cd_arg(char **args)
-{
-	
-}
 
 int	ft_cd(char **args, t_env *env)
 {
 	int		i;
 	char	cwd[PATH_MAX];
-	char	*home_path = NULL;
+	char	*home_path;
 
 	home_path = get_env_value(env, "HOME");
-	ft_printf("%s", home_path);
 	if (!home_path)
 		write (2, "cd: HOME not set\n", 17);
 	i = 1;
@@ -32,8 +27,21 @@ int	ft_cd(char **args, t_env *env)
 	{
 		chdir(home_path);
 		getcwd(cwd, PATH_MAX);
+		return (0);
+	}
+	if (args[1] && args[2])
+	{
+		ft_putendl_fd("cd: Too many arguments.\n", 2);
 		return (1);
 	}
-	cd_arg(args);
-	return (-1);
+	if (args[1])
+	{
+		if (!args[1])
+		{
+			write(2, "cd: directory not found.\n", 27);
+		}
+		chdir(args[1]);
+		getcwd(cwd, PATH_MAX);
+	}
+	return (1);
 }
